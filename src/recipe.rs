1020e9cb049b6338316206e6e05e839c8ee8eb27
rkhan113 <roha.khan@pdx.edu>
2025-06-11 15:26:36 -0700

    Small fixes
    Fixed a bunch of silly things!

--- src/recipe.rs
+++ src/recipe.rs
@@ -1,18 +1,24 @@
 use serde::Deserialize;
+use once_cell::sync::Lazy;
 
 #[derive(Deserialize)]
-
 
 // Recipe struct to represent a recipe
 pub struct Recipe {
     pub name: &'static str,
-    pub ingredients: &'static str,
+    pub ingredients: Vec<&'static str>, // CHANGED: now a vector of strings
     pub instructions: &'static str,
 }
 
 // A hardcoded example recipe
-pub const THE_RECIPE: Recipe = Recipe {
+// lazy-initialized (nothing else was working)
+pub static THE_RECIPE: Lazy<Recipe> = Lazy::new(|| Recipe {
     name: "PB&J Sandwich",
-    ingredients: "Bread, peanut butter, jelly",
-    instructions: "Spread peanut butter on bread. Spread jelly on another bread slice. Put them together. Enjoy!",
-};
+    ingredients: vec![
+        "2 slices of bread",
+        "2 tbsp peanut butter",
+        "2 tbsp jelly",
+    ],
+    instructions: "Spread peanut butter on one slice of bread. Spread jelly on the other. Combine. Enjoy!",
+});
+
